e2bb48c42366e46bf9d777ee5111991b28620f81
GregFJr <finney101690@gmail.com>
2025-02-24 12:00:39 -0700

    fixed bug with config file

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://thinking-tester-contact-list.herokuapp.com/',
-    specPatter: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/command.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
